3b01f466ec880fc9046927837e087bab0cb6e148
AFZL210 <tricktomade@gmail.com>
2023-10-30 12:19:18 +0530

    update cookie name

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(req: NextRequest) {
     const path = req.nextUrl.pathname;
-    const authCookie = process.env.DEV_COOKIE_NAME || "";
+    const authCookie = process.env.COOKIE_NAME || "";
     const isPrivatePath = path.startsWith('/setting') || path.startsWith('/dashboard') || path.startsWith('/link/');
     const token = req.cookies.get(authCookie)?.value || "";
 
